03913285ad1a984dcc460737e4f168b7cb7dfabd
Pawelsourcecode <164669831+Pawelsourcecode@users.noreply.github.com>
2024-03-29 21:45:25 +0100

    Initial commit

---
+++ part04-02_line/src/line.py
@@ -0,0 +1,7 @@
+# Write your solution here
+def line(x, y):
+    for _ in range(y):
+        print(y, end="")
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    line(5, "x")
