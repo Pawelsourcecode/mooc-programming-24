b57bb1e627d7731525a7740a8e5b7137d7c62352
Pawelsourcecode <164669831+Pawelsourcecode@users.noreply.github.com>
2024-03-30 01:48:46 +0100

    learning

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,15 @@
 # Write your solution here
+
+my_list = [1, 2, 3, 4, 5]
+
+while True:
+    index = int(input("Index: "))
+    
+    if index == -1:
+        break
+    if index < 0 or index >= len(my_list):
+        print("Out of range")
+        continue
+    val = int(input("New value: "))
+    my_list[index] = val
+    print(my_list)
