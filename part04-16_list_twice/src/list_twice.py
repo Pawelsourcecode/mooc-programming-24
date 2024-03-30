b9d331c4c7cfb21acd9acacb4aa691dd9575ec70
Pawelsourcecode <164669831+Pawelsourcecode@users.noreply.github.com>
2024-03-31 00:37:59 +0100

    learning

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,13 @@
 # Write your solution here
+
+list = []
+
+while True:
+    word = int(input("New item: "))
+    list.append(word)
+    if word == 0:
+        print("Bye!")
+        break
+    print(f"The list now: {list}")
+    print(f"The list in order: {sorted(list)}")
+    
--- part04-19_range_of_list/src/range_of_list.py
+++ part04-19_range_of_list/src/range_of_list.py
@@ -1,4 +1,9 @@
 # Write your solution here
+def range_of_list(list):
+    largest = max(list)
+    smallest = min(list)
+    return largest - smallest
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
