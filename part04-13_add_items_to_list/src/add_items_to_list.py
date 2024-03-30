d4f193eac439c97be5b9368e2757e78635f436ae
Pawelsourcecode <164669831+Pawelsourcecode@users.noreply.github.com>
2024-03-30 19:34:05 +0100

    learning

--- part04-13_add_items_to_list/src/add_items_to_list.py
+++ part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,11 @@
 # Write your solution here
+size = int(input("How many items: "))
+list = []
+i = 1
+while i <= size:
+    item = int(input(f"item{i}: "))
+    list.append(item)
+    i += 1
+    if i > size:
+        break
+print(list)
--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,18 @@
 # Write your solution here
+
+list = []
+while True:
+    print(f"The list is now {list}")
+    option = input("a(d)d, (r)emove or e(x)it: ")
+    if option == "d":
+        # Value of item is length of list + 1
+        item = len(list) + 1
+        list.append(item)
+    elif option == "r":
+        list.pop(len(list) - 1)
+    elif option == "x":
+        print("Bye!")
+        break
+    else:
+        continue
+
