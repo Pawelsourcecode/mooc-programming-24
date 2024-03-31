e491507ad26c76466a309964e4796d40d67226c8
Pawelsourcecode <164669831+Pawelsourcecode@users.noreply.github.com>
2024-03-31 01:46:49 +0100

    LEARNING

--- part04-25_sum_of_positives/src/sum_of_positives.py
+++ part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,1 +1,10 @@
 # Write your solution here
+
+def sum_of_positives(my_list:list):
+    sum = 0
+    for i in my_list:
+        if i > 0:
+            sum += i
+    return sum
+
+    
--- part04-28_distinct_numbers/src/distinct_numbers.py
+++ part04-28_distinct_numbers/src/distinct_numbers.py
@@ -1,1 +1,14 @@
 # Write your solution here
+
+def distinct_numbers(my_list:list):
+    results = []
+    for i in my_list:
+        if i not in results:
+            results.append(i)
+
+    results.sort()
+    return results
+
+if __name__ == "__main__":
+    my_list = [3, 2, 2, 1, 3, 3, 1]
+    print(distinct_numbers(my_list)) # [1, 2, 3]
