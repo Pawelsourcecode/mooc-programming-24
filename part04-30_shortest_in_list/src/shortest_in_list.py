e139a10aa4056ba13f157752b3025a35a8af19c9
Pawelsourcecode <164669831+Pawelsourcecode@users.noreply.github.com>
2024-04-01 17:02:51 +0200

    learning

--- part04-30_shortest_in_list/src/shortest_in_list.py
+++ part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,17 @@
 # Write your solution here
+
+def shortest(my_list:list):
+    short = my_list[0]
+    for i in my_list:
+        if len(i) <= len(short):
+              short = i
+    return short
+
+if __name__ == "__main__":
+    
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = shortest(my_list)
+    print(result)
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    result = shortest(my_list)
+    print(result)
--- part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -1,1 +1,20 @@
 # Write your solution here
+
+def all_the_longest(my_list:list):
+    longest = 0
+    newlist = []
+    for i in my_list:
+        if len(i) > longest:
+            longest = len(i)
+
+    for i in my_list:
+        if len(i) == longest:
+            newlist.append(i)
+
+    return newlist
+
+
+if __name__ == "__main__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = all_the_longest(my_list)
+    print(result) # ['eleventh']
