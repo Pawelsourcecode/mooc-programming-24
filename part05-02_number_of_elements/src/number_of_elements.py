4147019fd61ad6f78be1ad1d0ca30ee288e815f2
Pawelsourcecode <164669831+Pawelsourcecode@users.noreply.github.com>
2024-04-03 02:32:14 +0200

    learning

--- part05-02_number_of_elements/src/number_of_elements.py
+++ part05-02_number_of_elements/src/number_of_elements.py
@@ -1,1 +1,14 @@
 # Write your solution here
+
+def count_matching_elements(my_matrix: list, element: int):
+    matching = 0
+    for i in range(len(my_matrix)):
+        for j in range(len(my_matrix[i])):
+            if my_matrix[i][j] == element:
+                matching += 1
+    return matching
+
+
+if __name__ == "__main__":
+    m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+    print(count_matching_elements(m, 1))
--- part05-03_go/src/go.py
+++ part05-03_go/src/go.py
@@ -1,1 +1,21 @@
 # Write your solution here
+
+def who_won(game_board:list):
+    counter1 = 0
+    counter2 = 0
+    for i in game_board:
+        for j in i:
+            if j == 1:
+                counter1 += 1
+            elif j == 2:
+                counter2 += 1
+    if counter1 > counter2:
+        return 1
+    elif counter2 > counter1:
+        return 2
+    else:
+        return 0
+    
+if __name__ == "__main__":
+    go = [[1, 2, 2, 2], [2, 1, 1, 1], [0, 2, 1, 0]]
+    print(who_won(go))
