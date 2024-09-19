7ae56ebaa778f5e47cb858749554897df72ad936
Pawelsourcecode <164669831+Pawelsourcecode@users.noreply.github.com>
2024-09-19 02:21:45 +0200

    learning

--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,15 +1,18 @@
 # Write your solution here
-
 def factorials(n: int):
-    newdict = {}
+    my_dictionary = {}
     factorial = 1
-    for i in range(1, n+1):
+    
+    for i in range(1, n + 1):
         factorial *= i
-        newdict[i] = factorial
-    return newdict
+        my_dictionary[i] = factorial
+
+    return my_dictionary
 
 if __name__ == "__main__":
     k = factorials(5)
     print(k[1])
     print(k[3])
     print(k[5])
+
+    
